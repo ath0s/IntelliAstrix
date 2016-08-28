f14710ec489b3ebd0231cfdf5524da20381e8ced
Daniel Bergholm <daniel.bergholm@gmail.com>
2016-08-28 18:23:20 +0200

    Performance improvements through multithreading of AstrixBeanDeclarationLineMarker

--- src/main/java/com/avanza/astrix/intellij/AstrixBeanDeclarationLineMarker.java
+++ src/main/java/com/avanza/astrix/intellij/AstrixBeanDeclarationLineMarker.java
@@ -4,24 +4,25 @@ import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor;
 import com.intellij.codeInsight.daemon.MergeableLineMarkerInfo;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.concurrency.JobLauncher;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressIndicatorProvider;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiIdentifier;
 import com.intellij.psi.PsiMethod;
-import com.intellij.util.MergeQuery;
-import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 import static com.avanza.astrix.intellij.AstrixContextUtility.findBeanUsages;
 import static com.avanza.astrix.intellij.AstrixContextUtility.isBeanDeclaration;
-import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 public class AstrixBeanDeclarationLineMarker extends LineMarkerProviderDescriptor {
     private final Icon icon = Icons.Gutter.asterisk;
@@ -36,31 +37,41 @@ public class AstrixBeanDeclarationLineMarker extends LineMarkerProviderDescripto
     @Override
     public void collectSlowLineMarkers(@NotNull List<PsiElement> elements, @NotNull Collection<LineMarkerInfo> result) {
         ApplicationManager.getApplication().assertReadAccessAllowed();
-        elements.stream().map(this::collectNavigationMarkers).filter(Objects::nonNull).forEach(result::add);
+
+        if (beanOption.isEnabled()) {
+            final Object lock = new Object();
+            ProgressIndicator indicator = ProgressIndicatorProvider.getGlobalProgressIndicator();
+            JobLauncher.getInstance().invokeConcurrentlyUnderProgress(elements, indicator, true, element -> {
+                createLineMarkerInfo(element).ifPresent(info -> {
+                    synchronized (lock) {
+                        result.add(info);
+                    }
+                });
+                return true;
+            });
+        }
     }
 
-    @Nullable
-    private MergeableLineMarkerInfo<PsiElement> collectNavigationMarkers(@NotNull PsiElement element) {
+    private Optional<MergeableLineMarkerInfo> createLineMarkerInfo(@NotNull PsiElement element) {
         PsiElement parent;
-        if (element instanceof PsiIdentifier && beanOption.isEnabled() && (parent = element.getParent()) instanceof PsiMethod) {
+        if (element instanceof PsiIdentifier && (parent = element.getParent()) instanceof PsiMethod) {
             PsiMethod method = (PsiMethod) parent;
 
             if (isBeanDeclaration(method)) {
-                return NavigationGutterIconBuilder.create(icon)
-                        .setTargets(new NotNullLazyValue<Collection<? extends PsiElement>>() {
-                            @NotNull
-                            @Override
-                            protected Collection<? extends PsiElement> compute() {
-                                return findBeanUsages(method).stream()
-                                                             .reduce(MergeQuery::new)
-                                                             .map(Query::findAll)
-                                                             .orElse(emptyList());
-                            }
-                        })
-                        .createLineMarkerInfo(element);
+                return Optional.of(NavigationGutterIconBuilder.create(icon)
+                                                              .setTargets(new NotNullLazyValue<Collection<? extends PsiElement>>() {
+                                                                  @NotNull
+                                                                  @Override
+                                                                  protected Collection<? extends PsiElement> compute() {
+                                                                      return findBeanUsages(method).parallelStream()
+                                                                                                   .flatMap(query -> query.findAll().stream())
+                                                                                                   .collect(toList());
+                                                                  }
+                                                              })
+                                                              .createLineMarkerInfo(element));
             }
         }
-        return null;
+        return Optional.empty();
     }
 
     @Nullable
