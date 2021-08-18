1eb04efcecef091f2c80a1b3a4be35d3f7349243
ath0s <861482+ath0s@users.noreply.github.com>
2021-08-18 14:17:37 +0200

    Only suppress specified toolIds

--- src/main/java/com/avanza/astrix/intellij/AstrixBeanDeclarationUnusedInspectionSuppressor.kt
+++ src/main/java/com/avanza/astrix/intellij/AstrixBeanDeclarationUnusedInspectionSuppressor.kt
@@ -2,22 +2,28 @@ package com.avanza.astrix.intellij
 
 import com.intellij.codeInspection.InspectionSuppressor
 import com.intellij.codeInspection.SuppressQuickFix
+import com.intellij.codeInspection.deadCode.UnusedDeclarationInspectionBase.ALTERNATIVE_ID
+import com.intellij.codeInspection.deadCode.UnusedDeclarationInspectionBase.SHORT_NAME
 import com.intellij.concurrency.JobLauncher
 import com.intellij.openapi.progress.ProgressIndicatorProvider
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiMethod
 
 class AstrixBeanDeclarationUnusedInspectionSuppressor : InspectionSuppressor {
 
+    private val toolIds = setOf(SHORT_NAME, ALTERNATIVE_ID)
+
     override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
-        val method = (element as? PsiIdentifier)?.parent as? PsiMethod ?: return false
-        if (AstrixContextUtility.isBeanDeclaration(method)) {
-            return !JobLauncher.getInstance().invokeConcurrentlyUnderProgress(AstrixContextUtility.findBeanUsages(method), ProgressIndicatorProvider.getGlobalProgressIndicator()) {
-                it.findFirst() != null
-            }
+        if (toolId !in toolIds) {
+            return false
         }
-        return false
+        val method = element.parent as? PsiMethod ?: return false
+        if (!AstrixContextUtility.isBeanDeclaration(method)) {
+            return false
+        }
+        return !JobLauncher.getInstance().invokeConcurrentlyUnderProgress(AstrixContextUtility.findBeanUsages(method), ProgressIndicatorProvider.getGlobalProgressIndicator()) {
+            it.findFirst() != null
+        }
     }
 
     override fun getSuppressActions(element: PsiElement?, toolId: String) =
