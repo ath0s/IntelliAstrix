f0b4abb2319c4b837c67516a3e515eae8e5aa2a3
ath0s <861482+ath0s@users.noreply.github.com>
2022-02-14 19:02:08 +0100

    Update Gradle IntelliJ plugin to 1.4.0, see https://github.com/JetBrains/gradle-intellij-plugin/releases/tag/v1.4.0 Update Gradle Changelog plugin to 1.3.1, see https://github.com/JetBrains/gradle-changelog-plugin/releases/tag/v1.3.1 Update Gradle to 7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.1.4"
+    id("org.jetbrains.intellij") version "1.4.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "1.2.1"
+    id("org.jetbrains.changelog") version "1.3.1"
 }
 
 group = properties("pluginGroup")
@@ -83,5 +83,9 @@ tasks {
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+    }
+
+    wrapper {
+        gradleVersion = "7.4"
     }
 }
